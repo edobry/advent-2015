d09231f4a2afd9bbaac6723f54e0dba1da7c1652
Eugene Dobry <edory@gmail.com>
2015-12-07 00:33:09 -0500

    refactors

--- 2/part2.js
+++ 2/part2.js
@@ -1,21 +1,9 @@
 var fs = require("fs"),
     byline = require("byline");
 
-var area = 0;
-
 var parse = line => line.split('x').map(x => parseInt(x, 10));
 
 var isLast = (i, arr) => i == arr.length - 1;
-
-var multiplying = (total, val) => total * val;
-var summing = (total, val) => total + val;
-
-var surfaceArea = function(box) {
-    return 2 * box.reduce((total, dim, i, arr) =>
-        total + arr.slice(i+1)
-            .map(x => dim * x)
-            .reduce(summing, 0), 0);
-};
 
 var maxOf_i = arr => arr.reduce((max_i, x, i) => x > arr[max_i] ? i : max_i, 0);
 
@@ -28,15 +16,24 @@ var twoSmallest = tuple => tuple.reduce((smallest, val) => {
         return smallest;
     }, [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]);
 
-var smallestSide_area = function(box) {
-    return twoSmallest(box).reduce(multiplying, 1);
+var duplicate = x => [x,x];
+
+var flatten = (arr, el) => arr.concat(...el);
+
+var multiplying = (total, val) => total * val;
+var summing = (total, val) => total + val;
+
+var smallestSide_perimeter = function(box) {
+    return twoSmallest(box).map(duplicate).reduce(flatten).reduce(summing, 0);
 };
+
+var volume = box => box.reduce(multiplying, 1);
 
 var input = byline(fs.createReadStream("input", { encoding: "utf8" }));
 input.on("data", function(line) {
     var present = parse(line);
 
-    area += surfaceArea(present) + smallestSide_area(present);
+    area += volume(present) + smallestSide_perimeter(present);
 });
 
 input.on("end", function() {
